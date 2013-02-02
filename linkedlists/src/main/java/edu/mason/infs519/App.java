4cedf5ae7ac8752e8222797b16ee4ab1858adcba
jamaal.taylor <jamaal.taylor@six3systems.com>
2013-02-01 22:23:22 -0500

    Adding maven and the .gitignore file.

---
+++ linkedlists/src/main/java/edu/mason/infs519/App.java
@@ -0,0 +1,13 @@
+package edu.mason.infs519;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
